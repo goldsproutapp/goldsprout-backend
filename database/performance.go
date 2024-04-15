c9febb74904503c483336d44508f4d6ad406bdc0
Patrick Jones <git@patrickjones.uk>
2024-04-15 22:11:48 +0100

    Fetch performance data for all users for admin

--- database/performance.go
+++ database/performance.go
@@ -3,11 +3,15 @@ package database
 import (
 	"github.com/patrickjonesuk/investment-tracker-backend/auth"
 	"github.com/patrickjonesuk/investment-tracker-backend/models"
+	"github.com/patrickjonesuk/investment-tracker-backend/util"
 	"gorm.io/gorm"
 )
 
 func FetchPerformanceData(db *gorm.DB, user models.User) []models.StockSnapshot {
 	uids := auth.GetAllowedUsers(user, true, false)
+    if (user.IsAdmin) {
+        uids = util.UserIDs(GetAllUsers(db))
+    }
 	qry := db.Model(&models.StockSnapshot{}).
 		Where("user_id IN ?", uids).
         Order("date").
